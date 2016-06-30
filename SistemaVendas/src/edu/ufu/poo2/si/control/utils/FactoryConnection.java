4e10e216cd8764c74707f8fa122324806be94297
Arthur Lucena <arthurvmx5@gmail.com>
2016-06-29 21:18:50 -0300

    removendo warnings

--- SistemaVendas/src/edu/ufu/poo2/si/control/utils/FactoryConnection.java
+++ SistemaVendas/src/edu/ufu/poo2/si/control/utils/FactoryConnection.java
@@ -6,60 +6,60 @@ import java.sql.SQLException;
 
 public class FactoryConnection {
 
-    private static FactoryConnection thiz;
+	private static FactoryConnection thiz;
 
-    private FactoryConnection() {
-    }
+	private FactoryConnection() {
+	}
 
-    public static synchronized FactoryConnection getInstance() {
-        if (thiz == null)
-            thiz = new FactoryConnection();
+	public static synchronized FactoryConnection getInstance() {
+		if (thiz == null)
+			thiz = new FactoryConnection();
 
-        return thiz;
-    }
+		return thiz;
+	}
 
-    // Método de Conexão//
-    public Connection getConnection() {
-        Connection connection = null; // atributo do tipo Connection
+	// Método de Conexão//
+	public Connection getConnection() {
+		Connection connection = null; // atributo do tipo Connection
 
-        try {
-            // Carregando o JDBC Driver padrão
-            String driverName = "com.mysql.jdbc.Driver";
+		try {
+			// Carregando o JDBC Driver padrão
+			String driverName = "com.mysql.jdbc.Driver";
 
-            Class.forName(driverName);
+			Class.forName(driverName);
 
-            // Configurando a nossa conexão com um banco de dados//
-            String serverName = "localhost"; // caminho do servidor do BD
-            String mydatabase = "sv"; // nome do seu banco de dados
-            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
-            String username = "root"; // nome de um usuário de seu BD
-            String password = "root"; // sua senha de acesso
-            connection = DriverManager.getConnection(url, username, password);
+			// Configurando a nossa conexão com um banco de dados//
+			String serverName = "localhost"; // caminho do servidor do BD
+			String mydatabase = "sv"; // nome do seu banco de dados
+			String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+			String username = "root"; // nome de um usuário de seu BD
+			String password = "root"; // sua senha de acesso
+			connection = DriverManager.getConnection(url, username, password);
 
-            return connection;
-        } catch (ClassNotFoundException e) { // Driver não encontrado
-            System.out.println("O driver expecificado nao foi encontrado.");
+			return connection;
+		} catch (ClassNotFoundException e) { // Driver não encontrado
+			System.out.println("O driver expecificado nao foi encontrado.");
 
-            return null;
-        } catch (SQLException e) {
-            // Não conseguindo se conectar ao banco
-            System.out.println("Nao foi possivel conectar ao Banco de Dados.");
+			return null;
+		} catch (SQLException e) {
+			// Não conseguindo se conectar ao banco
+			System.out.println("Nao foi possivel conectar ao Banco de Dados.");
 
-            return null;
-        }
-    }
+			return null;
+		}
+	}
 
-    public boolean fecharConexao(Connection connection) {
-        try {
-            connection.close();
-            return true;
-        } catch (SQLException e) {
-            return false;
-        }
-    }
+	public boolean fecharConexao(Connection connection) {
+		try {
+			connection.close();
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+	}
 
-    public Connection reiniciarConexao(Connection connection) {
-        fecharConexao(connection);
-        return FactoryConnection.getInstance().getConnection();
-    }
+	public Connection reiniciarConexao(Connection connection) {
+		fecharConexao(connection);
+		return FactoryConnection.getInstance().getConnection();
+	}
 }
